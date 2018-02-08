018fff8dc31dda317a4cdd5136265d49b73293e8
Mitchel Humpherys <mitch@directangular.com>
2018-02-07 21:39:30 -0800

    Add field with callable default

--- src/polls/models.py
+++ src/polls/models.py
@@ -1,6 +1,14 @@
+import random
+
 from django.db import models
+
+
+# yes, this is dumb
+def get_my_uuid():
+    return random.randint(1, 999999)
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    myuuid = models.PositiveIntegerField(default=get_my_uuid)
