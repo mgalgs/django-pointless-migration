ac5b5ed6d5a8670b545141da9c053683fb32e9f3
Mitchel Humpherys <mitch@directangular.com>
2018-02-07 21:19:23 -0800

    Change callable for default
    This migration generates the following sql in sqlmigrate:
    
        BEGIN;
        --
        -- Alter field myuuid on question
        --
        COMMIT;
    
    which is exactly what we'd expect.

--- src/polls/models.py
+++ src/polls/models.py
@@ -8,7 +8,12 @@ def get_my_uuid():
     return random.randint(1, 999999)
 
 
+# yes, this is dumb
+def get_my_uuid2():
+    return random.randint(1, 10000000)
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
-    myuuid = models.PositiveIntegerField(default=get_my_uuid)
+    myuuid = models.PositiveIntegerField(default=get_my_uuid2)
